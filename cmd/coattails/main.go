62a7d347fa3de3074769352e12e48aec89f4198c
Jason Ma <jason.ma@airbnb.com>
2020-04-27 21:20:38 -0700

    ran gofmt, init func

--- cmd/coattails/main.go
+++ cmd/coattails/main.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"fmt"
-	//"github.com/bluedresscapital/coattails/pkg/calc"
+	"github.com/bluedresscapital/coattails/pkg/sundress"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"github.com/bluedresscapital/coattails/pkg/sundress"
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
+	plaintext := "1234"
+	log.Printf("plaintext: %s", plaintext)
+	cipher := sundress.Encrypt(plaintext)
+	log.Printf("cipher: %s", cipher)
+	decryptedCipher := sundress.Decrypt(cipher)
+	log.Printf("Decripted cipher: %s", decryptedCipher)
 	fmt.Fprintf(w, "Welcome home!!!")
 }
 
 func main() {
-	println(sundress.Decrypt(sundress.Encrypt("1234")))
 	router := mux.NewRouter().StrictSlash(true)
 	log.Print("Hello world!")
-	fmt.Print("Serving traffic on 8080")
 	router.HandleFunc("/", homeLink)
-	fmt.Print("Is this still working?")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- pkg/sundress/kms.go
+++ pkg/sundress/kms.go
@@ -1,59 +1,64 @@
 package sundress
 
 import (
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/kms"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"log"
 )
 
 type secret struct {
-	keyId *string
+	keyId  *string
 	client *kms.KMS
+}
+
+var (
+	sec *secret
+)
+
+func init() {
+	sec = newSecret()
 }
 
 func newSecret() *secret {
 	//need to change how credentials are stored, rn using my local
 	sess, err := session.NewSession(&aws.Config{
-        Region: aws.String("us-east-1")},
-    )
+		Region: aws.String("us-east-1")},
+	)
 
-    if err != nil{
-    	print("[ERROR] session initialization error")
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    svc := kms.New(sess)
+	svc := kms.New(sess)
 
-    //need to replace the key with bdc stuff
-    return &secret{keyId: aws.String("arn:aws:kms:us-east-1:460622542287:key/8b4b0689-b1f2-4fb0-90cd-11f8a0c080ca"),
-        				client: svc}
+	//need to replace the key with bdc stuff
+	return &secret{
+		keyId:  aws.String("arn:aws:kms:us-east-1:460622542287:key/8b4b0689-b1f2-4fb0-90cd-11f8a0c080ca"),
+		client: svc,
+	}
 }
-
-//not sure if this is correct or not
-var sec secret = *newSecret()
 
 func Encrypt(s string) string {
-    // Encrypt the data
-    result, err := sec.client.Encrypt(&kms.EncryptInput{
-        KeyId: sec.keyId,
-        Plaintext: []byte(s),
-    })
+	// Encrypt the data
+	result, err := sec.client.Encrypt(&kms.EncryptInput{
+		KeyId:     sec.keyId,
+		Plaintext: []byte(s),
+	})
 
-    if err != nil {
-        print("[ERROR] Got error encrypting data: ", err)
-        return ""
-    }
-
-    return string(result.CiphertextBlob)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(result.CiphertextBlob)
 }
 
-func Decrypt (s string) string {
+func Decrypt(s string) string {
 	result, err := sec.client.Decrypt(&kms.DecryptInput{
-        CiphertextBlob: []byte(s),
-    })
+		CiphertextBlob: []byte(s),
+	})
 
-    if err != nil {
-        print("[ERROR] Got error decrypting data: ", err)
-        return ""
-    }
-    return string(result.Plaintext)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(result.Plaintext)
 }
