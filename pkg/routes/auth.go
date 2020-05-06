a085bb8f9ef322751632b7511740e1641d4fe73c
Pastorales Liu <ralles.liu@gmail.com>
2020-05-05 23:01:33 -0400

    flags user registration error

--- pkg/routes/auth.go
+++ pkg/routes/auth.go
@@ -96,6 +96,7 @@ func loginRegisterHelper(w http.ResponseWriter, r *http.Request, loginMode bool)
 	} else {
 		tok, err = auth.Register(l.Username, cipherPwd)
 		if err != nil {
+			log.Printf("we boomed here: %v", err)
 			http.Error(w, err.Error(), http.StatusUnauthorized)
 			return
 		}
